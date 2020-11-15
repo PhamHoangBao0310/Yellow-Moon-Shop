c17fb21fc5bfde323f1e2e938aef03149120c853
Phạm Hoàng Bảo <55708231+PhamHoangBao0310@users.noreply.github.com>
2020-11-15 16:55:43 +0700

    Update GoogleHelper.java

--- src/java/baoph/utils/GoogleHelper.java
+++ src/java/baoph/utils/GoogleHelper.java
@@ -11,8 +11,8 @@ package baoph.utils;
  */
 public class GoogleHelper {
 
-    public static String GOOGLE_CLIENT_ID = "973378573756-eukbmptcvtbri72erh8le31441q97g3t.apps.googleusercontent.com";
-    public static String GOOGLE_CLIENT_SECRET = "eKKWEPdHqQzX8OkRu0cPnv_U";
+    public static String GOOGLE_CLIENT_ID = "";
+    public static String GOOGLE_CLIENT_SECRET = "";
     public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/yellowMoonShop/login-google";
     public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
     public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
